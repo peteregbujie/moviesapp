2c391cb9a8178fb8af37d11282c4d52292e8d941
Peter Egbujie <ogbillion@gmail.com>
2025-02-08 15:23:44 -0800

    set up application structure; add routing, API and React Query integration, pages, hooks and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import  { Home, MovieDetails, SearchResults } from './pages';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+    },
+  },
+});
 
-function App() {
- 
+const App = () => {
   return (
-    <>
-      <h1>Movie App</h1>
-    </>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <Router>     
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/:id" element={<MovieDetails />} />
+            <Route path="/search" element={<SearchResults />} />
+          </Routes>   
+      </Router>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
 
 export default App
---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+import Home from './Home';
+import MovieDetails from './MovieDetails';
+import SearchResults from './SearchResults';
+
+export {
+Home,
+ MovieDetails,
+ SearchResults,
+};
