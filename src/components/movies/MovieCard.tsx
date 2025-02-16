6bc933329d4fa61e6c0168a70f5aa4551390c89d
Peter Egbujie <ogbillion@gmail.com>
2025-02-15 17:02:20 -0800

    feat: Update MovieCard component to display movie posters from TMDB API

--- src/components/movies/MovieCard.tsx
+++ src/components/movies/MovieCard.tsx
@@ -1,6 +1,8 @@
 
-import {  Star,  } from 'lucide-react';
+import { Star } from 'lucide-react';
 import { Movie } from '../../types/movie.types';
+
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 // MovieCard Component
 const MovieCard = ({ movie }: { movie: Movie }) => {
@@ -8,7 +10,7 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
     <div className="relative group overflow-hidden rounded-lg shadow-lg bg-gray-800 transition-transform duration-300 hover:-translate-y-1">
       <div className="aspect-[2/3] relative">
         <img 
-          src="/api/placeholder/300/450"
+          src={movie.poster_path ? `${IMAGE_BASE_URL}${movie.poster_path}` : '/no-poster.png'}
           alt={movie.title}
           className="w-full h-full object-cover"
         />
