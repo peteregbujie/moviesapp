e888a02f0a66eddfc259aeca545259fd65f71f74
Peter Egbujie <ogbillion@gmail.com>
2025-02-15 18:56:40 -0800

    feat: Add click navigation to MovieCard component for movie details

--- src/components/movies/MovieCard.tsx
+++ src/components/movies/MovieCard.tsx
@@ -1,13 +1,23 @@
 
 import { Star } from 'lucide-react';
 import { Movie } from '../../types/movie.types';
+import { useNavigate } from 'react-router-dom';
 
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 // MovieCard Component
 const MovieCard = ({ movie }: { movie: Movie }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/movie/${movie.id}`);
+  };
+
   return (
-    <div className="relative group overflow-hidden rounded-lg shadow-lg bg-gray-800 transition-transform duration-300 hover:-translate-y-1">
+    <div 
+      onClick={handleClick}
+      className="relative group overflow-hidden rounded-lg shadow-lg bg-gray-800 transition-transform duration-300 hover:-translate-y-1 cursor-pointer"
+    >
       <div className="aspect-[2/3] relative">
         <img 
           src={movie.poster_path ? `${IMAGE_BASE_URL}${movie.poster_path}` : '/no-poster.png'}
@@ -19,7 +29,7 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
             <h3 className="text-white font-semibold">{movie.title}</h3>
             <div className="flex items-center space-x-2">
               <Star className="w-4 h-4 text-yellow-400" />
-              <span className="text-white">{movie.vote_average}</span>
+              <span className="text-white">{movie.vote_average.toFixed(1)}</span>
             </div>
           </div>
         </div>
